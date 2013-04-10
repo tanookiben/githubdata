03099ca898cd535755c6b3349e3e14b72d6fb4d9
Benjamin Hsieh <bhsiehh@gmail.com>
2013-04-09 22:18:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Githubdata::Application.config.secret_token = '653e6969ebf944901310d1514f652d9ae4b7e360e743b9d93ef3b11590758faaf1d73569b0703da2a98d2ada7bb452d8f41a58d985b7a7e1e1acec0c52c0e7da'
