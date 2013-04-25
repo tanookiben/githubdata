29e82762e8446bb98be77b92744630eab424006d
Benjamin Hsieh <bhsiehh@gmail.com>
2013-04-24 23:13:19 -0700

    refactored some code paths and added more skrollr options to times visualization

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 //= require skrollr
 //= require twitter/bootstrap
 //= require d3
+//= require_directory ./default
---
+++ app/assets/javascripts/times.js
@@ -0,0 +1,36 @@
+$(function() {
+  $("#star1").css("top","10%");
+  $("#star1").css("left","20%");
+  
+  $("#star2").css("top","70%");
+  $("#star2").css("left","20%");
+  
+  $("#star3").css("top","40%");
+  $("#star3").css("left","30%");
+  
+  $("#star4").css("top","90%");
+  $("#star4").css("left","50%");
+  
+  $("#star5").css("top","80%");
+  $("#star5").css("left","40%");
+  
+  $("#star6").css("top","50%");
+  $("#star6").css("left","90%");
+  
+  $("#star7").css("top","10%");
+  $("#star7").css("left","70%");
+  
+  $("#star8").css("top","30%");
+  $("#star8").css("left","80%");
+  
+  $("#star9").css("top","60%");
+  $("#star9").css("left","80%");
+ 
+  $("#star10").css("top","80%");
+  $("#star10").css("left","20%");
+  
+  $("#star11").css("top","10%");
+  $("#star11").css("left","80%");
+    
+	var s = skrollr.init();
+});
